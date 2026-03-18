eae7b94c00d3e614562184aade8a22e640c3bf70
gdghanu-Taii <gdschanu.taipham@gmail.com>
2026-03-18 22:47:32 +0700

    WeatherProject-Initial

---
+++ src/Component/NavBar/NavBar.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import logoImg from '../../assets/Logo/LogoforWeb.png';
+
+const NavBar = () => {
+  
+  return (
+    <div>
+      <div>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default NavBar
